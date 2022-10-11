9915e03f0edf22683028db16d61ca797d605b9cc
gabrielgv456 <gabrielgv4569@gmail.com>
2022-10-11 14:25:46 -0300

    adding horizontal and radar charts

--- src/index.js
+++ src/index.js
@@ -392,10 +392,10 @@ app.post("/charts/bestsellers", async (request, response) => {
             })
 
         )
-        
-        const firstsSellers = Sellers.filter((value,index)=> index <= 3)
+
+        const firstsSellers = Sellers.filter((value, index) => index <= 3)
         firstsSellers.sort(function (x, y) { return y.totalValueSell - x.totalValueSell }) // odernar array
-        
+
         if (Sellers) {
             return response.json({ Success: true, bestSellers: firstsSellers })
         }
@@ -405,6 +405,102 @@ app.post("/charts/bestsellers", async (request, response) => {
     }
     catch (error) {
         return response.json({ Success: false, erro: error })
+    }
+})
+
+app.post("/charts/topsellingproducts", async (request, response) => {
+    try {
+        const { userId } = request.body
+        const atualMonth = new Date().getMonth() + 1
+        const atualYear = new Date().getFullYear()
+        const initialDate = new Date(atualMonth > 9 ? `${atualYear}-${atualMonth}-01T03:00:00.000Z` : `${atualYear}-0${atualMonth}-01T03:00:00.000Z`)
+        const finalDate = new Date(initialDate.getFullYear(), initialDate.getMonth() + 1, 0)
+
+
+        const topSellingProducts = await prisma.itensSell.groupBy({
+            where: {
+                AND: [{
+                    created_at: {
+                        gt: initialDate
+                    }
+                },
+                {
+                    created_at: {
+                        lt: finalDate
+                    }
+                },
+                { storeId: userId },
+                ]
+            },
+            _sum: { quantity: true },
+            by: ['idProduct'],
+            orderBy: { _sum: { quantity: 'desc' } },
+            take: 5
+        })
+
+        await Promise.all(
+            topSellingProducts.map(async sell => {
+                sell.quantity = sell._sum.quantity
+                const findProducts = await prisma.products.findUnique({
+                    where: { id: sell.idProduct }
+                })
+                sell.productName = findProducts.name
+                delete sell._sum
+            }
+            )
+        )
+        return response.json({Success:true, topSellingProducts })
+    }
+    catch (error) {
+        return response.json({ Success: false, erro: error })
+    }
+})
+
+app.post("/charts/radar", async(request,response) => {
+    try{
+        const {userId} = request.body
+        const atualMonth = new Date().getMonth() + 1
+        const atualYear = new Date().getFullYear()
+        const initialDate = new Date(atualMonth > 9 ? `${atualYear}-${atualMonth}-01T03:00:00.000Z` : `${atualYear}-0${atualMonth}-01T03:00:00.000Z`)
+        const finalDate = new Date(initialDate.getFullYear(), initialDate.getMonth() + 1, 0)
+
+        const Payments = await prisma.paymentSell.groupBy({
+            where: {
+                AND: [{
+                    created_at: {
+                        gt: initialDate
+                    }
+                },
+                {
+                    created_at: {
+                        lt: finalDate
+                    }
+                },
+                { storeId: userId },
+                ]
+            },
+            _count: { id: true },
+            by: ['typepayment'],
+            orderBy: { typepayment: 'asc' },
+            take: 5
+        })
+
+        await Promise.all (
+            Payments.map(payment=>{
+                if (payment.typepayment === 'money'){payment.typepayment = 'Dinheiro'}
+                if (payment.typepayment === 'creditcard'){payment.typepayment = 'Cartão de Crédito'}
+                if (payment.typepayment === 'debitcard'){payment.typepayment = 'Cartão de Débito'}
+                if (payment.typepayment === 'pix'){payment.typepayment = 'Pix'}
+                if (payment.typepayment === 'others'){payment.typepayment = 'Outros'}
+                payment.quantity = payment._count.id
+                delete payment._count
+            })
+        )
+
+        return response.json({Success:true, Payments})
+    }
+    catch(error) {
+        return response.json({Success:false, erro: error})
     }
 })
 // END HOME //
